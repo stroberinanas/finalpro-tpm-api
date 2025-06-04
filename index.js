df9615cbd6c5fd920d733e8b7163144ea4316147
stroberinanas <sabilakhairina2@gmail.com>
2025-06-04 18:15:55 +0700

    update photo

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
-import userRoute from "./routes/UserRoute.js"; // import routing
+import userRoute from "./routes/UserRoute.js";
+import path from "path";     // import routing
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // Pakai routes user
 app.use(userRoute);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
